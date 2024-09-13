de45c038d30dc91e2df7e7210e450e17b519cce3
raisrasyiid <raisrasyiid01@gmail.com>
2024-09-13 13:10:58 +0700

    `feat`: if statement

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,6 +30,10 @@
     <div class="container-fluid">
         <h1>Selamat datang {{$name}}</h1>
         <h2>Role anda adalah {{$role}}</h2>
+
+        @if ($role == 'admin')
+            <p>selamat anda memiliki semua hak akses disini!</p>
+        @endif
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
