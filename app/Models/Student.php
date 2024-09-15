aabbd6af9705b600843ddd5ae8072bfb19a0fb6d
raisrasyiid <raisrasyiid01@gmail.com>
2024-09-15 22:13:56 +0700

    `refactor`: add insight on model

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+
+    // !!!jika nama table ini dalam bentuk plural, maka syntax dibawah ini tidak perlu dituliskan. !!!
+    // protected $table = 'students';
+
+    // !!!jika nama kolom key 'id', maka syntax dibawah ini tidak perlu dituliskan. !!!
+    // protected $primaryKey = 'id';
 }
