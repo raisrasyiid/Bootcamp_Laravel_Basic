40008f4ba873262492cc61d758ce78df78f26690
raisrasyiid <raisrasyiid01@gmail.com>
2024-09-17 19:39:34 +0700

    `feat`: create data with faker

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use Faker\Factory as Faker;
+use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +18,14 @@ class StudentFactory extends Factory
      */
     public function definition()
     {
+        $faker = Faker::create();
         return [
-            //
+            'fullname' => $faker->name(),
+            'gender' => Arr::random(['L', 'P']),
+            'nis' => mt_rand(00000001, 99999999),
+            'class_id' => Arr::random([1, 2, 3, 4]),
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 }
