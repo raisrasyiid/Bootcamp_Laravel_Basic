7e073ef222c0b563ad4517d2e569589931267b66
raisrasyiid <raisrasyiid01@gmail.com>
2024-09-18 11:35:54 +0700

    `refactor`: eloquent orm

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -10,35 +10,10 @@ class StudentController extends Controller
 {
     public function index()
     {
-
-        // --- QUERY BUILDER ---
-        //get 
-        // $data = DB::table('students')->get();
-        // return view('students', ['students' => $data]);
-
-        //post 
-        // DB::table('students')->insert([
-        //     'fullname' => 'query builder',
-        //     'gender' => 'L',
-        //     'nis' => '21023365',
-        //     'class_id' => 1
-        // ]);
-
-        //put 
-        // DB::table('students')
-        //     ->where('id', 22)
-        //     ->update(['fullname' => 'andy eloquent']);
-
-        //delete 
-        // DB::table('students')->where('id', 22)->delete();
-
-
-
-
         //--- ELOQUENT ORM ---
         //get
-        // $data = Student::all();
-        // return view('students', ['students' => $data]);
+        $data = Student::all();
+        return view('students', ['students' => $data]);
 
         //post
         // Student::create([
