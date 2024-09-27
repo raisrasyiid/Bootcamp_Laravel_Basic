94b64dca7b29e640a15a9e937940753dff8eaf9d
raisrasyiid <raisrasyiid01@gmail.com>
2024-09-27 16:40:01 +0700

    feat: seeder extra

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ClassSeeder::class,
             StudentSeeder::class,
+            ExtraSeeder::class
         ]);
     }
 }
