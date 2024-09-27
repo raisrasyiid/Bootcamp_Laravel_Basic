89c587bb199c0d5ee2da58a5173c83dd93fe4ac8
raisrasyiid <raisrasyiid01@gmail.com>
2024-09-27 16:40:26 +0700

    feat: new model extra

---
+++ app/Models/Extra.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Extra extends Model
+{
+    use HasFactory;
+
+    protected $table = 'extras';
+
+    public function students()
+    {
+        return $this->belongsToMany(Student::class, 'student_extra', 'extra_id', 'students_id');
+    }
+}
