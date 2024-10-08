0085f141009c49f680c6ed24d2fee8d2bd32ea62
raisrasyiid <raisrasyiid01@gmail.com>
2024-10-08 19:14:59 +0700

    feat: add new view detail students

---
+++ resources/views/students/students_detail.blade.php
@@ -0,0 +1,53 @@
+@extends('layout.template')
+@section('title', 'Students_Detail')
+
+@section('content')
+<div class="container-fluid">
+        <h1>Data Mahasiswa {{$student->fullname}}</h1>
+        <table class="table table-bordered mt-5">
+            <thead class="text-center">
+                <tr>
+                    {{-- <th>NO</th> --}}
+                    <th>NIM</th>
+                    <th>Nama</th>
+                    <th>Jenis Kelamin</th>
+                    <th>Kelas</th>
+                    <th>Wali Kelas</th>
+                </tr>
+            </thead>
+            <tbody>
+                    <tr>
+                        <td>{{$student->nis}}</td>
+                        <td>{{$student->fullname}}</td>
+                       
+                        <td>
+                            @if ($student->gender == 'L')
+                            Laki-laki
+                        @else
+                            Perempuan
+                        @endif
+                        </td>
+                        <td>{{$student->class->name}}</td>
+                        <td>{{$student->class->homeroomTeacher->name}}</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <div class="container">
+                <h4>Extrakurikuler</h4>
+                <ol>
+                    @if($student->extra->isEmpty())
+                    Tidak mengikuti extrakurikuler
+                @else
+                    @foreach ($student->extra as $item)
+                        <li>{{$item->name}}</li>
+                    @endforeach
+                @endif
+                </ol>
+            </div>
+            <br>
+            <a href="/students" class="btn btn-primary btn-sm">Back</a>
+      </div>
+
+     
+@endsection
