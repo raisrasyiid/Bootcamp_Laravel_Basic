b0f0f569065eb09a61924b31b7507f77ed61454b
raisrasyiid <raisrasyiid01@gmail.com>
2024-10-27 16:09:09 +0700

    `feat` : create message error when field data updated not complete

--- resources/views/teacher/teacher_add.blade.php
+++ resources/views/teacher/teacher_add.blade.php
@@ -4,12 +4,21 @@
 @section('content')
 <div class="container-fluid">
         <h1>Form tambah data Teacher</h1>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
        <div class="mt-5 col-6 m-auto">
         <form action="teacher" method="POST">
             @csrf
             <div class="control-group mb-3">
                 <label for="teacher">Nama Teacherkurikuler</label>
-                <input type="text" name="name" class="form-control" id="teacher" placeholder="Masukan nama teacher anda..." required>
+                <input type="text" name="name" class="form-control" id="teacher" placeholder="Masukan nama teacher anda...">
             </div>
             <div class="control-group mb-3">
                 <button type="submit" class="btn btn-success">Simpan</button>
